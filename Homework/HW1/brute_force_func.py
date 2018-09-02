32fa832169ba2596a068f5fa02311ed9736e51cd
bafasse <blakefasse@gmail.com>
2018-09-02 12:06:44 -0500

    Finished with homework

--- Homework/HW1/brute_force_func.py
+++ Homework/HW1/brute_force_func.py
@@ -1,3 +1,5 @@
+import encrypt_decrypt
+
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def brute_force():
@@ -6,16 +8,22 @@ def brute_force():
 
     for key in range(len(alphabet)):
         plain_text = ''
-        for symbol in user_message:
-            if symbol in alphabet:
-                num = alphabet.find(symbol)
+        for letter in user_message:
+            if letter in alphabet:
+                num = alphabet.find(letter)
                 num -= key
 
                 if num < 0:
                     num += len(alphabet)
 
-                plain_text = plain_text + alphabet[num]
+                plain_text += alphabet[num]
             else:
-                plain_text = plain_text + symbol
+                plain_text += letter
+
+            # print(letter)
+
+            # if encrypt_decrypt.decrypt(key, user_message) == plain_text:
+            #     right_key = key
 
         print('Key #%s: %s' % (key, plain_text))
+        # print(encrypt_decrypt.decrypt(key, user_message))
